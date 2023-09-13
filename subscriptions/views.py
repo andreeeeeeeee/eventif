465ae13bd052cf39fdd00f44557c675e27259e4d
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2023-09-13 04:40:22 +0000

    view para inscrição

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,9 +1,28 @@
+from django.template.loader import render_to_string
+from subscriptions.form import SubscriptionForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from django.shortcuts import render
+from django.conf import settings
+from django.core import mail
 
 
 def subscribe(request):
     if request.method == 'POST':
-        return render(request, "subscriptions/subscription_form.html")
-        # se for inscrever lidar cm isso
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            subject = 'Confirmação de inscrição'
+            from_email = settings.DEFAULT_FROM_EMAIL
+            email = form.cleaned_data['email']
+            template_name = 'subscriptions/subscription_email.txt'
+            context = form.cleaned_data
+
+            body = render_to_string(template_name, context)
+            mail.send_mail(subject, body, from_email, [from_email, email])
+
+            messages.success(request, 'Inscrição realizada com sucesso!')
+            return HttpResponseRedirect('/inscricao/')
     else:
-        return render(request, "subscriptions/subscription_form.html")
+        form = SubscriptionForm()
+
+    return render(request, 'subscriptions/subscription_form.html', {'form': form})
