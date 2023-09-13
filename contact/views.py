cc6e559683af36b5a0d9366f52c11aa10a10e7f3
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2023-09-13 05:21:03 +0000

    view do contact criado e url pronta

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .form import ContactForm
+from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        formulario = ContactForm(request.POST)
+
+        if formulario.is_valid():
+            sender_email = formulario.cleaned_data['email']
+            recipient_emails = [settings.DEFAULT_FROM_EMAIL, sender_email]
+            subject = 'EventIF - Contato'
+            message_body = render_to_string(
+                'contact_email.txt', {'contact_data': formulario.cleaned_data})
+            mail.send_mail(subject, message_body,
+                           sender_email, recipient_emails)
+
+            messages.success(request, 'Mensagem enviada!')
+            return redirect('/contact/')
+    else:
+        formulario = ContactForm()
+
+    return render(request, 'contact_form.html', {'form': formulario})
--- eventif/urls.py
+++ eventif/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from core.views import home
 from subscriptions.views import subscribe
+from contact.views import contact
 
 urlpatterns = [
     path('', home),
     path('inscricao/', subscribe),
     path("admin/", admin.site.urls),
+    path("inscricao/", contact),
+
 ]
